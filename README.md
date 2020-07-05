0f9abfb007ed5f0f4d75a0b7283c5c82c2167f04
wacky@Zacs-Desktop <zcraven17@gmail.com>
2020-07-05 13:14:56 -0700

    fixed promise error

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Weather-Board
+## Upon putting in city it draws forcast out
+## gives pictures of weather
--- assets/js/java.js
+++ assets/js/java.js
@@ -1,23 +1,24 @@
-var cityOutput = function (cityName) {
+function cityOutput () {
+    var searchTerm = document.querySelector("#searchTerm").value;
     //format the weather app api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily"+ cityName +"&appid=00f364721c3c88adc95bd446bfd8b6cf";
+    var cityId = document.querySelector("#cityId");
 
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayWeather(cityId, cityName)
-            console.log(data);
-        })
+    fetch (
+        "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily" +
+        searchTerm +
+        "#cityId" +
+        cityId +
+        "&appid=00f364721c3c88adc95bd446bfd8b6cf"
+    )
+    .then(function(response) {
+        return response.json();
     })
-   
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=00f364721c3c88adc95bd446bfd8b6cf").then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
-            console.log("inside",response);
+    .then(function(data){
+        console.log(data);
+        var responseContainerEl = document.querySelector("#response-container");
+        responseContainerEl.innerHTML = '';
     })
-    console.log("outside");
-};
-
+    };
 var cityFormEL = document.querySelector("#cityName");
 var nameInputEL = document.querySelector("#city")
 
@@ -29,7 +30,7 @@ var formSubmitHandler = function(event) {
 
     if(city) {
         cityOutput(city);
-        nameInputEL.value == "";
+        nameInputEL.value === "";
     }
     else {
         alert("Incorrect city")
@@ -39,21 +40,21 @@ var formSubmitHandler = function(event) {
 
 cityFormEL.addEventListener("submit", formSubmitHandler)
 
-var response = fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=00f364721c3c88adc95bd446bfd8b6cf");
-console.log(response);
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=00f364721c3c88adc95bd446bfd8b6cf")
+.then(function(response) {
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    var responseContainerEl = document.querySelector("#response-container");
+    responseContainerEl.innerHTML = '';
+})
 
-
-var displayWeather = function(city, cityName) {
+// Displays weather
+var displayWeather = function(data, city) {
+    console.log(data);
     console.log(city);
-    console.log(cityName)
+    response.json().then(function(data) {
+        displayWeather(data, city)
+    })
 };
-
-// api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&amp;appid={your api key}
-
-// var apiURL = 'api.openweathermap.org/data/2.5/weather?id={city id}&appid=00f364721c3c88adc95bd446bfd8b6cf' + searchTerm;
-
-// fetch ('http//api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid=00f364721c3c88adc95bd446bfd8b6cf' ) .then(function(response) {
-//     return response.json();
-//     }).then(function(results) {
-//         console.log('results', results)
-//     });
